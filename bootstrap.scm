98325d2180b1065df2d9b2bbd293f023847469b1
griezmannFromJavic <119756145+griezmannFromJavic@users.noreply.github.com>
2025-11-14 13:28:50 +0100

    added foldl and reverse

--- bootstrap.scm
+++ bootstrap.scm
@@ -4,7 +4,6 @@
 (define length (lambda (lst) (if (null? lst) 0 (+ 1 (length (cdr lst))))))
 (define append (lambda (lst1 lst2) (if (null? lst1) lst2 (cons (car lst1) (append (cdr lst1) lst2)))))
 
-; iterators
 (define foldr
     (lambda (fun terminator lst)
         (if
@@ -14,6 +13,18 @@
         )
     )
 )
+
+(define foldl
+    (lambda (fun terminator lst)
+        (if
+            (null? lst)
+            terminator
+            (foldl fun (fun (car lst) terminator) (cdr lst))
+        )
+    )
+)
+
+(define reverse (lambda (lst) (foldl cons (quote ()) lst)))
 
 ; boolean operations
 (define not (lambda (x) (if (null? x) #t #f)))
